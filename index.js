113e9976b58fe5a868f6ca0632baa726a1626132
Divyani Mittal <hellodivyani@gmail.com>
2017-05-17 10:49:55 +0530

    Added Video Code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+navigator.webkitGetUserMedia({video:true,audio:false},function(stream){
 var Peer = require('simple-peer');
 var peer = new Peer({
 	initiator: location.hash === '#init',
-	trickle: false
-	
+	trickle: false,
+	stream : stream
 })
 
 peer.on('signal',function(data)
@@ -15,19 +16,32 @@ document.getElementById('connect').addEventListener('click',function()
 {
 	var otherId = JSON.parse(document.getElementById('OtherId').value);
 	peer.signal(otherId);
-});
+})
 
 document.getElementById('send').addEventListener('click',function()
 {
 	
 	var MyMessage = document.getElementById('myMessage').value;
 	peer.send(MyMessage);
-	alert('Sending');
-});
+	
+})
 
 peer.on('data',function(data)
 {
-	alert('Receiving');
+	
 	document.getElementById('messages').textContent += data + '\n';
 	
+})
+peer.on('stream',function(stream)
+{
+	var video = document.createElement('video')
+	document.body.appendChild(video)
+	video.src = window.URL.createObjectURL(stream)
+	video.play();
+	
+})
+
+},function(err)
+{
+	console.error(err);
 });
